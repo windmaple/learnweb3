b3222ba5ba1c06d2e0fc884c60d1c24d6605d4e0
Wei Wei <weiwe@google.com>
2022-05-10 17:31:09 +0800

    Add code for NFTCollection in Sophomore track

---
+++ Sophomore/NFTCollection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xEA67a1b72AcB71c7422578a6edfa0a822239E39a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
