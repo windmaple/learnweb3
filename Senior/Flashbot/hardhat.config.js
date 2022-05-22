e6617e1088630b5488df826f04e12b3fc85ea3d4
Wei Wei <weiwe@google.com>
2022-05-22 21:32:31 +0800

    Add Senior track Flashbot

---
+++ Senior/Flashbot/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
+
+// 0x9aD53125741939b955a4D944eF3c630518A97C28
